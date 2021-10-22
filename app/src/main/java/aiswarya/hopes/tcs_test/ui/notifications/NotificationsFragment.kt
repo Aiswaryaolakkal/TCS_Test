31939f4b457c5ec96a293cf716b40d8b47d5d4bb
TCS project <aiswaryaplus@gmail.com>
2021-10-23 00:01:00 +0530

    initial commit

--- app/src/main/java/aiswarya/hopes/tcs_test/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/aiswarya/hopes/tcs_test/ui/notifications/NotificationsFragment.kt
@@ -13,6 +13,7 @@ import aiswarya.hopes.tcs_test.R
 import aiswarya.hopes.tcs_test.databinding.FragmentHomeBinding
 import aiswarya.hopes.tcs_test.databinding.FragmentNotificationsBinding
 import aiswarya.hopes.tcs_test.databinding.FragmentSettingsBinding
+import androidx.navigation.fragment.findNavController
 
 class NotificationsFragment : Fragment() {
     private lateinit var binding: FragmentNotificationsBinding
@@ -31,6 +32,9 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             binding.textNotifications.text = it
         })
+        binding.textNotifications.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_notifications_to_detailViewFragment)
+        }
         return view
     }
 
--- app/src/main/java/aiswarya/hopes/tcs_test/ui/search/SearchAdapter.kt
+++ app/src/main/java/aiswarya/hopes/tcs_test/ui/search/SearchAdapter.kt
@@ -1,5 +1,6 @@
 package aiswarya.hopes.tcs_test.ui.search
 
+import aiswarya.hopes.tcs_test.R
 import aiswarya.hopes.tcs_test.model.BookEntity
 import aiswarya.hopes.tcs_test.databinding.BookItemLayoutBinding
 import androidx.recyclerview.widget.RecyclerView
@@ -7,6 +8,10 @@ import android.view.ViewGroup
 
 import android.util.Log
 import android.view.LayoutInflater
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 
@@ -14,6 +19,7 @@ import androidx.recyclerview.widget.ListAdapter
  *Book Adapter
  */
 class SearchAdapter : ListAdapter<BookEntity, SearchAdapter.SearchViewHolder>(DiffCallback()) {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
         val binding = BookItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -27,10 +33,24 @@ class SearchAdapter : ListAdapter<BookEntity, SearchAdapter.SearchViewHolder>(Di
 
     class SearchViewHolder(private val binding: BookItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(bookDB: BookEntity) {
+        fun bind(bookEntity: BookEntity) = with(itemView) {
+            // TODO: Bind the data with View
+
+            setOnClickListener {
+//                val bundle = bundleOf("amount" to amount)
+//                findNavController().navigate(R.id.action_navigation_search_to_detailViewFragment, bundle)
+                val data = bundleOf(
+                    "book_id" to bookEntity.id,
+                    "book_name" to bookEntity.bookName,
+                    "author_name" to bookEntity.authorName
+                )
+                findNavController().navigate(R.id.action_navigation_search_to_detailViewFragment,data)
+
+            }
+
             binding.apply {
-                nameTv.text = bookDB.bookName
-                authorNameTV.text = bookDB.authorName
+                nameTv.text = bookEntity.bookName
+                authorNameTV.text = bookEntity.authorName
             }
         }
     }
